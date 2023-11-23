4d9e7513f8062d1d805f774394acb50daa811514
hrvthdavidgyorgy <54977557+hrvthdavidgyorgy@users.noreply.github.com>
2023-11-23 15:34:42 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -17,3 +17,20 @@ st.write("Operation")
  
 operation = st.radio("Select an operation to perform:",
                     ("Add", "Subtract", "Multiply", "Divide"))
+
+ans = 0
+ 
+def calculate():
+    if operation == "Add":
+        ans = num1 + num2
+    elif operation == "Subtract":
+        ans = num1 - num2
+    elif operation == "Multiply":
+        ans = num1 * num2
+    elif operation=="Divide" and num2!=0:
+        ans = num1 / num2
+    else:
+        st.warning("Division by 0 error. Please enter a non-zero number.")
+        ans = "Not defined"
+ 
+    st.success(f"Answer = {ans}")
