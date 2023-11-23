c27f17fbb16460917372f23f11b68a452f0a17ba
hrvthdavidgyorgy <54977557+hrvthdavidgyorgy@users.noreply.github.com>
2023-11-23 15:33:55 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,7 +8,12 @@ st.title("Calculator App using Streamlit")
 st.write("---")
 
 # input 1
-num1 = st.number_input(label="Enter first number")
+num1 = st.number_input(label="ird be az eslö számot")
  
 # input 2
-num2 = st.number_input(label="Enter second number")
+num2 = st.number_input(label="ird be a második számot")
+
+st.write("Operation")
+ 
+operation = st.radio("Select an operation to perform:",
+                    ("Add", "Subtract", "Multiply", "Divide"))
