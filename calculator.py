a7ee8608345bbd3e3fb93f5ca4aeacc3d9ed8c87
hrvthdavidgyorgy <54977557+hrvthdavidgyorgy@users.noreply.github.com>
2023-11-23 15:32:48 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,4 +5,10 @@ import streamlit as st
 st.title("Calculator App using Streamlit")
  
 # creates a horizontal line
-st.write("--")
+st.write("---")
+
+# input 1
+num1 = st.number_input(label="Enter first number")
+ 
+# input 2
+num2 = st.number_input(label="Enter second number")
